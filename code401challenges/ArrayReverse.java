5e105c2b5257f659c3ff6eb49fe413606754e0a7
Kevin Couture <kcouture939@gmail.com>
2019-08-06 07:45:49 -0700

    Added 401 cc 1

---
+++ code401challenges/ArrayReverse.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class ArrayReverse {
+  public static void main(String[] args) {
+    System.out.println("We are about to reverse an array!");
+    int[] inArr = new int[]{1,2,3,4,5,6,7};
+    reverseArray(inArr);
+  }
+
+  public static int[] reverseArray(int[] inArr) {
+    System.out.println("The input array is: " + Arrays.toString(inArr));
+    int temp;
+    int len = inArr.length;
+    // Travel through half of the array swapping with a mirrored index. (ie: first and last are "mirrored")
+    for(int i = 0; i < len/2; i++) {
+        // Swap
+        temp = inArr[i];
+        inArr[i] = inArr[len-i-1];
+        inArr[len-i-1] = temp;
+    }
+    System.out.println("The output array is: " + Arrays.toString(inArr));
+    return inArr;
+  }
+}
